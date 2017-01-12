bac30e5d8ee6ed9cb9a90a375eae9a9e08fb62bd
Oleksandr Mihal <oleksandr.mihal@gmail.com>
2017-01-12 16:24:08 +0200

    Update

--- Task_3_1_3/MyClass.java
+++ Task_3_1_3/MyClass.java
@@ -26,14 +26,13 @@ public class MyClass implements Serializable
         return strValue;
     }
 
-    public int getIndex()
-    {
-        return index;
-    }
-
     public void setIndex(int index)
     {
         this.index = index;
+    }
+    public int getIndex()
+    {
+        return index;
     }
 
     public void setCounter(int counter)
